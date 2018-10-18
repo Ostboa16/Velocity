4e82b56dd22fe56614fbf84d0e0b53ce5bd89b7e
Boris <ostboa16@htlkaindorf.at>
2018-10-18 15:35:11 +0200

    Add und Remove

--- Beispiel/src/data/VelocityTableModel.java
+++ Beispiel/src/data/VelocityTableModel.java
@@ -8,6 +8,7 @@ import javax.swing.table.AbstractTableModel;
 public class VelocityTableModel extends AbstractTableModel {
 
     private List<VelocityBL> liste = new ArrayList<>();
+    public static final String[] colNames = {"Datum", "Uhrzeit", "Kennzeichen", "Gemessen", "Erlaubt", "Übertretung"};
     
     @Override
     public int getRowCount() {
@@ -16,12 +17,34 @@ public class VelocityTableModel extends AbstractTableModel {
 
     @Override
     public int getColumnCount() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return colNames.length;
+    }
+    
+    @Override
+    public String getColumnName(int colIndex) {
+        return colNames[colIndex];
     }
 
     @Override
-    public Object getValueAt(int i, int i1) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Object getValueAt(int rowIndex, int colIndex) {
+       VelocityBL v = liste.get(rowIndex);
+        switch(colIndex){
+            case 0: return v.getDatum();
+            case 1: return v.getUhrzeit();
+            case 2: return v.getKennzeichen();
+            case 3: return v.getGemessen();
+            case 4: return v.getErlaubt();
+            case 5: return v.getUebertretung();
+        }
+        return "?";
+    }
+    
+    public void add(VelocityBL v){
+        liste.add(v);
+    }
+    
+    public void remove(int i){
+        liste.remove(i);
     }
     
 }
